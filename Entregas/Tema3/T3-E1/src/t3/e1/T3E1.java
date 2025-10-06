a2a5799a0832f9f8d1cda6657782ff7d2b36caa2
1DAM - Lorena Palomino Galindo <lpalominog01@informaticavalledeljerteplasencia.es>
2025-10-06 20:57:26 +0200

    Ej 06/10
    Por terminar, tema 3 ejercicio1

---
+++ Entregas/Tema3/T3-E1/src/t3/e1/T3E1.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package t3.e1;
+
+import java.text.BreakIterator;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lorena
+ * Pídele al usuario un número e impreime si es positivo o negativo
+ */
+public class T3E1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int numUsuario;
+        
+        Scanner entrada = new Scanner (System.in);
+        System.out.println("Por favor, introduzca el numero");
+        numUsuario = entrada.nextInt();
+        
+            if (numUsuario >0){
+                    System.out.println("El numero es positivo");
+                    else if(numUsuario == 0);
+                    System.out.println("El numero es positivo");
+                else (numUsuario < 0);
+                    System.out.println ("El numero es negativo");
+             }
+        
+        
+    }
+    
+}
